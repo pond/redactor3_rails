9927ad49e5b25f8c940776bd940c6a736f7e402e
marsz <marsz330@gmail.com>
2018-08-16 17:55:37 +0800

    fix

--- app/controller/redactor3_rails/files_controller.rb
+++ app/controller/redactor3_rails/files_controller.rb
@@ -1,5 +1,6 @@
 class Redactor3Rails::FilesController < Redactor3Rails.base_controller
   include ActionView::Helpers::NumberHelper
+  skip_before_action :verify_authenticity_token
   before_action :redactor3_authenticate_user!
 
   def create
